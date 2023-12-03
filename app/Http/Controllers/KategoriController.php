732224ca320596da91e8539731aa3d259c823892
yusuf <diandrayusuf23@gmail.com>
2023-12-03 17:45:56 +0700

    Membuat Routes dan mengisi controller

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class KategoriController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $data = Kategori::all('CategoryID', 'CategoryName')->toArray();
+        var_dump($data);
+    }
 }
--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produk;
 use Illuminate\Http\Request;
 
 class ProdukController extends Controller
 {
-    //
+    public function index($id)
+    {
+        $data = Produk::where('CategoryID', $id)->get('ProductID', 'ProductName', 'UnitPrice')->toArray();
+        var_dump($data);
+    }
+
+    public function detail($id)
+    {
+        $data = Produk::where('ProductID', $id)->get()->toArray();
+        var_dump($data);
+    }
 }
